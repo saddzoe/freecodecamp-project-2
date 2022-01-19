b32ca434f815fcf3ebb9dcefc08167b7b767abe8
Mohammed Olaiya <mohammedolaiya@gmail.com>
2022-01-18 23:14:59 -0500

    Update and rename README.md to README.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# freecodecamp-project-2
-This is a project I did from freecodecamp
---
+++ README.py
@@ -0,0 +1,8 @@
+# freecodecamp-project-2
+# This is a project I did from freecodecamp
+
+import numpy as np
+
+def calculate(list):
+
+   if (len(list) != 9):
